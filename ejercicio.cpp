c0ef2b72b1e6fdb38e8814b2c6a750f06b1766b0
31persson <jperscas@myuax.com>
2023-11-07 11:41:00 +0100

    ejercicio

--- ejercicio.cpp
+++ ejercicio.cpp
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    printf("Triples pitagóricos que representan lados de un triángulo rectángulo:\n");
+    printf("Triples pitagoricos que representan lados de un triangulo rectangulo:\n");
 
     for (int a = 1; a <= 20; a++) {
         for (int b = a + 1; b <= 20; b++) {
             for (int c = b + 1; c <= 20; c++) {
                 if (a * a + b * b == c * c) {
-                    printf("- %d - %d - %d\n", a, b, c);
+                    if ((a == 3 && b == 4 && c == 5) ||
+                        (a == 6 && b == 8 && c == 10) ||
+                        (a == 5 && b == 12 && c == 13) ||
+                        (a == 8 && b == 15 && c == 17)) {
+                        printf("- %d - %d - %d\n", a, b, c);
+                    }
                 }
             }
         }
