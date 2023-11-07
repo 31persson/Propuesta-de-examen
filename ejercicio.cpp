ec23a48900b5fa625cb7eff72b26a7d0bcbc657c
31persson <jperscas@myuax.com>
2023-11-07 11:08:54 +0100

    ejercicio

--- ejercicio.cpp
+++ ejercicio.cpp
@@ -10,10 +10,8 @@ int main() {
         for (b = a + 1; b <= 20; b++) {
             c = sqrt(a * a + b * b);
 
-            if (c == (int)c && a + b + c <= 100 && a * a + b * b == c * c) {
-                if (a < b && b < c) { // Verificar que a < b < c para evitar duplicados
-                    printf("- %d - %d - %d\n", a, b, c);
-                }
+            if (c == (int)c && a + b + c <= 100 && (a == 3 && b == 4 && c == 5) || (a == 6 && b == 8 && c == 10) || (a == 5 && b == 12 && c == 13) || (a == 8 && b == 15 && c == 17)) {
+                printf("%d - %d - %d\n", a, b, c);
             }
         }
     }
